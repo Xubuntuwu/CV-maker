01786a25f758eb890617e1db16782fed0fdf957c
Xubuntuwu <uwuxubuntu@gmail.com>
2022-06-24 14:52:47 +0200

    education section styling finished, still left: language, skills, experience

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Education.css';
 class Education extends React.Component{
     constructor(props){
         super();
@@ -10,13 +11,12 @@ class Education extends React.Component{
         return(
             <div id="cveducation">
                 <h3>Education</h3>
-                {/* key={this.generateKey(`this.props.educationarray[${i}].schooldate`)} */}
                 {this.props.educationarray.map(function(object, i){
                     return <div key={this.generateKey(i)}>
                     <ul>
-                        <li>{this.props.educationarray[i].degree}</li>
-                        <li>{this.props.educationarray[i].school}</li>
-                        <li>{this.props.educationarray[i].schooldate}</li>
+                        <li id="cvdegree">{this.props.educationarray[i].degree}</li>
+                        <li id="cvschool">{this.props.educationarray[i].school}</li>
+                        <li id="cvschooldate">{this.props.educationarray[i].schooldate}</li>
                     </ul>
                 </div>
                 }, this)}
